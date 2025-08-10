8557a10d5431d56ddb7e49affa4a2cb849133402
ega <202210027@fit.edu.ph>
2025-08-10 10:40:21 +0800

    chore: initial commit

---
+++ src/utils/calcu.js
@@ -0,0 +1,14 @@
+
+
+export const calculateCronbachAlpha = (e) => {
+    console.log(e);
+    let k;
+    let s2y;
+    let total_s2y = 0;
+    let s2i;
+    
+    //todo cronbach logic
+
+
+    return 1;
+};
