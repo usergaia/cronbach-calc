fe8eb1ad642fb4a7dbe798ae6522e1e4968a1c5a
ega <202210027@fit.edu.ph>
2025-08-12 10:13:13 +0800

    wip: placeholder guide

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -1,6 +1,6 @@
 export const Guide = () => {
     return (
-        <div class="w-1/3 bg-gray-200 p-4 mr-10 text-black rounded-lg">
+        <div class="w-1/3 bg-gray-200 p-4 mr-10 text-black rounded-lg self-auto">
             <h2>Guide</h2>
             <p>This is a guide to using the Cronbach's Alpha Calculator.</p>
         </div>
